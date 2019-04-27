7f136a2c7bc51132871d08144b827f3cc33a7186
Gabriel Alcantara <gabrielalcantaratp@gmail.com>
2019-04-27 10:31:42 -0300

    feat: procedural.

---
+++ procedural/index.php
@@ -0,0 +1,12 @@
+<?php 
+    $mensagem = 'Hello Word';
+    echo $mensagem;
+    $a = 5;
+    $b = 6;
+    $soma = $a+$b;
+    print $soma;
+
+    if($a % 2 == 0)
+        echo "S";
+    else
+        echo "N";
---
+++ procedural/switch.php
@@ -0,0 +1,10 @@
+<?php
+    $n = 10;
+    switch($n){
+        case 1:
+            echo 'hi';
+            break;
+        case 10:
+            echo 'oi';
+            break;
+    }
